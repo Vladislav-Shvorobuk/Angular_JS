f98c859029d99ad98faedd6e8450f16adf0dd495
Vladislav <vladislav.shvorobuk@gmail.com>
2019-02-12 10:51:06 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+(function() {
+  class AngularJS {
+    directive() {
+      // write code
+    }
+    compile() {
+      // write code
+    }
+    bootstrap() {
+      // write code
+    }
+  }
+
+  window.angular = new AngularJS();
+}());
