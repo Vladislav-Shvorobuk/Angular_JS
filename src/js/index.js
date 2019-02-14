127091e1affb07a00209eeab8079445f40db47ef
Vladislav <vladislav.shvorobuk@gmail.com>
2019-02-14 12:26:32 +0200

    branch fix/methods 1) fix method compile - divide atributs on dirs and other 2) add throw Error to method directive

--- src/js/index.js
+++ src/js/index.js
@@ -5,58 +5,58 @@
     }
 
     directive(name, func) {
+      if (this.directives[name]) {
+        throw Error('This directive has already registered.');
+      }
       this.directives[name] = func;
     }
 
-    compile(nodeEl) {
-      const attrs = nodeEl.attributes;
-      const nodeDirectives = {};
+    compile(node) {
+      const attrs = node.attributes;
+      const dirs = [];
+      const otherAttrs = [];
 
       for (let i = 0; i < attrs.length; i++) {
         if ((/[ng-]/).test(attrs[i].name)) {
-          nodeDirectives[attrs[i].name] = attrs[i].value;
+          const dir = this.directives[attrs[i].name];
+
+          if (dir) {
+            dirs.push(dir);
+          }
+        } else {
+          otherAttrs.push(attrs[i]);
         }
       }
 
-      for (const directive in nodeDirectives) {
-        const dir = this.directives[directive];
-
-        if (dir) {
-          dir(nodeEl);
-        }
-      }
+      dirs.forEach(dir => dir(node, otherAttrs));
     }
 
-    bootstrap(node = '[ng-app]') {
-      const parentNode = document.querySelector(node);
+    bootstrap(node) {
+      const parentNode = node ? node : document.querySelector('[ng-app]');
       const childNodes = parentNode.querySelectorAll('*');
       this.compile(parentNode);
       childNodes.forEach(node => this.compile(node));
     }
   }
 
+  /* eslint-disable no-console */
   function ngInit(el) {
-    // eslint-disable-next-line no-console
     console.log('called directive ng-init on element', el);
   }
 
   function ngShow(el) {
-    // eslint-disable-next-line no-console
     console.log('called directive ng-show on element', el);
   }
 
   function ngModel(el) {
-    // eslint-disable-next-line no-console
     console.log('called directive ng-model on element', el);
   }
 
   function ngMakeShort(el) {
-    // eslint-disable-next-line no-console
     console.log('called directive ng-make-short on element', el);
   }
 
   function ngBind(el) {
-    // eslint-disable-next-line no-console
     console.log('called directive ng-bind on element', el);
   }
 
