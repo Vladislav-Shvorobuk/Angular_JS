95cd4e07cd1cb0ca3baef7c296a63994a752fe34
Vladislav <vladislav.shvorobuk@gmail.com>
2019-02-14 16:01:12 +0200

    branch features 1) add watchers, add rootScope 2) change compile

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,16 @@
   class AngularJS {
     constructor() {
       this.directives = {};
+      this.watchers = [];
+      this.rootScope = window;
+
+      this.rootScope.$watch = (name, watcher) => {
+        this.watchers.push({ name, watcher });
+      };
+
+      this.rootScope.$apply = () => {
+        this.watchers.forEach(({ watcher }) => watcher());
+      };
     }
 
     directive(name, func) {
@@ -18,10 +28,16 @@
 
       for (let i = 0; i < attrs.length; i++) {
         const dir = this.directives[attrs[i].name];
-        dir ? dirs.push(dir) : otherAttrs.push(attrs[i]);
-      }
 
-      dirs.forEach(dir => dir(node, otherAttrs));
+        if (dir) {
+          dirs.push(dir);
+        }
+
+        if (!(/[ng-]/).test(attrs[i].name)) {
+          otherAttrs.push(attrs[i]);
+        }
+      }
+      dirs.forEach(dir => dir(this.rootScope, node, otherAttrs, 5));
     }
 
     bootstrap(node) {
@@ -33,32 +49,43 @@
   }
 
   /* eslint-disable no-console */
-  function ngInit(el) {
-    console.log('called directive ng-init on element', el);
+  /*  eslint-disable no-eval */
+  function ngInit(scope, node, attrs) {
+    const data = eval(node.getAttribute('ng-init'));
+    scope.$watch = (data, () => scope.data);
   }
 
-  function ngShow(el) {
-    console.log('called directive ng-show on element', el);
+  function ngShow(scope, node, attrs) {
+    console.log('called directive ng-show on element', scope);
+    console.log('called directive ng-show on element', node);
+    console.log('called directive ng-show on element', attrs);
   }
 
-  function ngModel(el) {
-    console.log('called directive ng-model on element', el);
+  function ngHide(scope, node, attrs) {
+    console.log('called directive ng-show on element', scope);
+    console.log('called directive ng-show on element', node);
+    console.log('called directive ng-show on element', attrs);
   }
 
-  function ngMakeShort(el) {
-    console.log('called directive ng-make-short on element', el);
-  }
+  // function ngModel(el) {
+  //   console.log('called directive ng-model on element', el);
+  // }
 
-  function ngBind(el) {
-    console.log('called directive ng-bind on element', el);
-  }
+  // function ngMakeShort(el) {
+  //   console.log('called directive ng-make-short on element', el);
+  // }
+
+  // function ngBind(el) {
+  //   console.log('called directive ng-bind on element', el);
+  // }
 
   const myAngular = new AngularJS();
   myAngular.directive('ng-init', ngInit);
   myAngular.directive('ng-show', ngShow);
-  myAngular.directive('ng-model', ngModel);
-  myAngular.directive('ng-make-short', ngMakeShort);
-  myAngular.directive('ng-bind', ngBind);
+  myAngular.directive('ng-hide', ngHide);
+  // myAngular.directive('ng-model', ngModel);
+  // myAngular.directive('ng-make-short', ngMakeShort);
+  // myAngular.directive('ng-bind', ngBind);
   myAngular.bootstrap();
 
   window.angular = myAngular;
